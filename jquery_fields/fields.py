705d4b24f172dded914ee39a6f89f44826cafa5b
Roman Akinfold <akinfold@gmail.com>
2013-04-06 02:33:28 +0800

    add implementation of TokenInputWidget, TokenInputField & ModelTokenInputField

--- jquery_fields/fields.py
+++ jquery_fields/fields.py
@@ -1,1 +1,44 @@
 # -*- coding: utf-8 -*-
+from django.forms import ModelMultipleChoiceField, MultipleChoiceField
+from jquery_fields.widgets import TokenInputWidget
+
+
+class TokenInputFieldMixin(object):
+    widget = TokenInputWidget
+
+    def __init__(self, choices, json_source, configuration=None, *args, **kwargs):
+        if 'widget' not in kwargs or kwargs['widget'] is None:
+            kwargs['widget'] = self.widget(json_source, configuration)
+        super(TokenInputFieldMixin, self).__init__(choices, *args, **kwargs)
+
+    def clean(self, value):
+        if value:
+            return super(TokenInputFieldMixin, self).clean(value.split(','))
+        else:
+            return []
+
+    def prepare_value(self, value):
+        if value:
+            value = value.split(',')
+        value = super(TokenInputFieldMixin, self).prepare_value(value)
+        return value
+
+
+class TokenInputField(TokenInputFieldMixin, MultipleChoiceField):
+    def __init__(self, queryset, json_source, *args, **kwargs):
+        super(TokenInputField, self).__init__(queryset, json_source, *args, **kwargs)
+
+
+class ModelTokenInputField(TokenInputFieldMixin, ModelMultipleChoiceField):
+    choices = []    # choices are always equal to field value, look into 'prepare_value' implementation
+
+    def __init__(self, queryset, json_source, *args, **kwargs):
+        super(ModelTokenInputField, self).__init__(queryset, json_source, *args, **kwargs)
+
+    def prepare_value(self, value):
+        # setup widget choices to current field value
+        choices = []
+        for obj in self.clean(value):
+            choices.append((super(ModelTokenInputField, self).prepare_value(obj), self.label_from_instance(obj)))
+        self.widget.choices = choices
+        return super(ModelTokenInputField, self).prepare_value(value)
--- jquery_fields/widgets.py
+++ jquery_fields/widgets.py
@@ -1,1 +1,56 @@
 # -*- coding: utf-8 -*-
+import json
+from django.forms import Textarea
+from django.forms.widgets import flatatt
+from django.utils.safestring import mark_safe
+
+
+class TokenInputWidget(Textarea):
+    choices = []
+    template = u'''
+<textarea%(attrs)s></textarea>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#%(id)s').tokenInput('%(json_source)s', %(configuration)s);
+    });
+</script>
+'''
+
+    class Media:
+        css = {
+            'all': (
+                'jquery_fields/tokeninput/styles/token-input.css',
+                'jquery_fields/tokeninput/styles/token-input-facebook.css',
+                'jquery_fields/tokeninput/styles/token-input-mac.css',
+            ),
+        }
+        js = (
+            'jquery_fields/tokeninput/src/jquery.tokeninput.js',
+        )
+
+    def __init__(self, json_source, configuration=None, attrs=None):
+        """
+        'json_source' url where tokeninput can get JSON choices.
+        'configuration' dict which will be directly sent to tokenInput constructor as second argument.
+
+        For more info about 'json_source' and 'configuration' look http://loopj.com/jquery-tokeninput/
+        """
+        self.json_source = json_source
+        self.configuration = configuration or {}
+        super(Textarea, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        if 'prePopulate' not in self.configuration:
+            self.configuration['prePopulate'] = []
+
+        if value is not None:
+            self.configuration['prePopulate'].extend([{'id': v, 'name': label} for v, label in self.choices])
+
+        final_attrs = self.build_attrs(attrs, name=name)
+        context = {
+            'id': final_attrs.get('id', '_'),
+            'attrs': flatatt(final_attrs),
+            'json_source': self.json_source,
+            'configuration': json.dumps(self.configuration)
+        }
+        return mark_safe(self.template % context)
